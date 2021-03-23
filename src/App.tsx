38ba934784c7f804e5b98d7c261c6bb7268aa143
JunYoung Wang <logtve@naver.com>
2021-03-23 21:31:49 +0900

    a

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,39 @@ import React, {CSSProperties} from 'react';
 import styled from 'styled-components';
 
 import './sub/callweather.ts';
-import ColdBG from './image/ColdBg.jpg';
-import WormBG from './image/WormBg.jpg';
+
+
+//날씨가 15도 이상이면 worm 이하면 cold
+const Container = styled.div`
+  //background: url(./image/WormBg.jpg);
+  //background-image: url("./image/WormBg.jpg");
+  background-size: cover;
+  position: center;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`
+
 
 const FontBox = styled.div`
   flex: 1;
-  font-family: "Roboto-italic";
-  font-size: 30px;
+  font-family: "Roboto Light";
+  font-size: 50px;
 `
 
 function App() {
   return (
 
       <FontBox>
-          <img src={WormBG} className={WormBG}/>
-          <header>
+          <Container>
               <div>
                   날씨
               </div>
-          </header>
-          <body>
-          <div>
+              <div>
               날씨2
-          </div>
-          </body>
+              </div>
+          </Container>
       </FontBox>
 
   );
