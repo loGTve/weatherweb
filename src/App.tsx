ca9342d4fdc457a83c50bc8d00a241abf56e8c47
JunYoung Wang <logtve@naver.com>
2021-03-24 20:35:27 +0900

    w
    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React, {CSSProperties, useEffect, useState} from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
+import {Search} from '@styled-icons/boxicons-regular/Search';
 
-//import './sub/callweather.ts';
-import WormBG from './image/WormBg.jpg';
-import ColdBG from './image/ColdBg.jpg';
-
-
+interface WeatherResponse{
+    data: WeatherData
+}
 
 type WeatherData = {
     temp: number;
@@ -17,50 +16,73 @@ type WeatherData = {
     temp_max: number;
 }
 
-/*function WeatherCall(){
+function WeatherCall(){
+
     const UrlApi = `http://api.openweathermap.org/data/2.5/forecast?q=seoul&cnt=1&appid=a0ec87e9b660428c2d1a0e9ef0cddfc4`;
-    const Weathers = axios.get(UrlApi).then((data) => data.data).catch((err) => console.error(err));
+    const Weathers = axios.get<{temp: number; temp_min: number;}>(UrlApi)
+        .then((data) => data.data)
+        .catch((err) => console.error(err));
 
-    return Weathers.
+
+    return Weathers;
 }
-*/
-
 
 
 
 
 //날씨가 15도 이상이면 worm 이하면 cold
 const Container = styled.div`
-  background: url(${WormBG});
-  background-size: cover;
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
+  display: flex;
+  position: relative;
+  margin: 0;
+  padding: 0;
+  
 `
 
-
 const FontBox = styled.div`
-  flex: 1;
   font-family: "Roboto Light";
   font-size: 50px;
 `
 
-function App() {
+const SearchBar = styled.input`
+  padding: 15px;
+  text-align: left;
+  border-radius: 25px;
+  background: #ffffff;
+  box-shadow: 3px 5px 20px rgba(0, 0, 0, 0.5);
+`
 
-  return (
-    <Container>
-      <FontBox>
-              <div>
-                  hello????????????????????
-              </div>
-              <div>
+const Box = styled.div`
+  width: 100%;
+  padding-top: 30px;
+  padding-bottom: 30px;
+  padding-left: 50px;
+  padding-right: 50px;
+`
 
-              </div>
-      </FontBox>
-    </Container>
-  );
+const SearchIcon = styled(Search)`
+  float: right;
+  width: 20px;
+  margin-top: 2px;
+  margin-right: 10px;
+  color: white;
+`
+
+
+function App(){
+
+    return (
+        <Container>
+            <FontBox>
+                <div>
+                   aa
+                </div>
+                <Box>
+                    <SearchBar type={"text"}/>
+                </Box>
+            </FontBox>
+        </Container>
+    );
 }
 
 export default App;
